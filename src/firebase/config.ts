6a012e25a25158d506e4949c1dfd394cf5c4235b
Prakashbabu Pramhans <divyahanssuperpower@gmail.com>
2026-03-17 16:43:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2772727041-39d56",
+  "appId": "1:472248677188:web:9871d63c04476e32905d11",
+  "apiKey": "AIzaSyB1w9HCFapyyzDA7MVYGl07Xhwf6YXWnyE",
+  "authDomain": "studio-2772727041-39d56.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "472248677188"
+};
