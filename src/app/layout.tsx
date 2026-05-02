9e4ab9daf9790aa071a096f9b91a1e98e426f865
Prakashbabu Pramhans <divyahanssuperpower@gmail.com>
2026-05-02 13:48:32 +0000

    esh web app ko mobile , laptop, tablets, etc. me logo ke sath bilkul app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#FF4F00" />
       </head>
       <body className={cn('min-h-screen bg-background font-body antialiased')}>
         <ThemeProvider
